30efafa33e321de98944e554ed66f643e76226f0
shiyao1122 <shiyao1122@gmail.com>
2026-02-16 12:40:00 +0800

    fix: add missing pinia dependencies and config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt']
 })
